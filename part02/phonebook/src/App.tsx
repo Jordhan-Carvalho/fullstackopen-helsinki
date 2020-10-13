a4521a0f5c1ce2f8e97b494c8d799081170da06a
Jordhan <jordhan.rdz@gmail.com>
2020-10-13 16:35:32 -0300

    2.9 Phonebook step4

--- part02/phonebook/src/App.tsx
+++ part02/phonebook/src/App.tsx
@@ -4,50 +4,59 @@ import './App.css';
 
 function App() {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: "883-3423-34" }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
-  const [ newName, setNewName ] = useState('')
-  const [ number, setNumber ] = useState('')
 
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewName(event.currentTarget.value);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const [formData, setFormData] = useState({number: "", name: ""})
+  const {name, number} = formData;
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({...formData, [e.currentTarget.name]: e.currentTarget.value});
   }
 
-  const handleNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNumber(event.currentTarget.value);
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.currentTarget.value);
   }
-
-
-
+  
   const addName = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
+    
+
+    if (persons.some(person => person.name === name)) {
+      alert(`${name} is already added to phonebook`);
       return;
     }
-    const newPersonToAdd = { name: newName, number};
+    const newPersonToAdd = { name, number};
     setPersons([...persons, newPersonToAdd])
-    setNewName('');
+    // setformData;
   }
 
-  console.log("rendered")
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input name="search" value={searchTerm} onChange={handleSearch} /></div>
+      <h2>Add a new</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          name: <input name="name" value={name} onChange={handleChangeInput} />
         </div>
         <div>
-          number: <input value={number} onChange={handleNumberChange} />
+          number: <input name="number" value={number} onChange={handleChangeInput} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {persons
+      .filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
+      .map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
